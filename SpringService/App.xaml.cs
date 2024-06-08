4e42e898e78a31c10febb46b10fdb592a41527b5
HP <HP@DESKTOP-68T114R>
2024-06-07 23:12:21 -0700

    Add project files.

---
+++ SpringService/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SpringService
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SpringService/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SpringService
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
